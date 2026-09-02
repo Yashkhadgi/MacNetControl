8a45e37acd56233e34e90e4a124f51f5a0ed514b
Yashkhadgi <yashkhadgi4@gmail.com>
2026-09-02 16:32:54 +0530

    feat: add Xcode network controller project

---
+++ NetworkController/NetworkController/NetworkControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkControllerApp.swift
+//  NetworkController
+//
+//  Created by Yash khadgi on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct NetworkControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkController/NetworkFilter/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  NetworkFilter
+//
+//  Created by Yash khadgi on 02/09/26.
+//
+
+import Foundation
+import NetworkExtension
+
+autoreleasepool {
+    NEProvider.startSystemExtensionMode()
+}
+
+dispatchMain()
